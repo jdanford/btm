f2ddd732d4765cf1ad8edbc78090b5d5ca112206
Jordan Danford <jordandanford@gmail.com>
2017-07-18 13:58:38 -0700

    Clean up ternary tests

--- src/tests/constants.rs
+++ src/tests/constants.rs
@@ -7,3 +7,12 @@ pub const TRYTE_0: Tryte = Tryte(0b00_00_00_00_00_00);
 pub const TRYTE_1: Tryte = Tryte(0b00_00_00_00_00_01);
 pub const TRYTE_64: Tryte = Tryte(0b00_01_11_01_00_01);
 pub const TRYTE_MAX: Tryte = Tryte(0b01_01_01_01_01_01);
+
+pub const TERNARY_MIN: [Tryte; 4] = [TRYTE_MIN, TRYTE_MIN, TRYTE_MIN, TRYTE_MIN];
+pub const TERNARY_NEG1: [Tryte; 4] = [TRYTE_NEG1, TRYTE_0, TRYTE_0, TRYTE_0];
+pub const TERNARY_0: [Tryte; 4] = [TRYTE_0, TRYTE_0, TRYTE_0, TRYTE_0];
+pub const TERNARY_1: [Tryte; 4] = [TRYTE_1, TRYTE_0, TRYTE_0, TRYTE_0];
+pub const TERNARY_MAX: [Tryte; 4] = [TRYTE_MAX, TRYTE_MAX, TRYTE_MAX, TRYTE_MAX];
+
+pub const WORD_MIN: i64 = -141_214_768_240;
+pub const WORD_MAX: i64 = 141_214_768_240;
--- src/tests/ternary.rs
+++ src/tests/ternary.rs
@@ -5,58 +5,33 @@ use tests::constants::*;
 
 #[test]
 fn ternary_into_i64() {
-    assert_eq!(
-        WORD_MIN,
-        into_i64(vec![TRYTE_MIN, TRYTE_MIN, TRYTE_MIN, TRYTE_MIN])
-    );
-    assert_eq!(-1, into_i64(vec![TRYTE_NEG1, TRYTE_0, TRYTE_0, TRYTE_0]));
-    assert_eq!(0, into_i64(vec![TRYTE_0, TRYTE_0, TRYTE_0, TRYTE_0]));
-    assert_eq!(1, into_i64(vec![TRYTE_1, TRYTE_0, TRYTE_0, TRYTE_0]));
-    assert_eq!(
-        WORD_MAX,
-        into_i64(vec![TRYTE_MAX, TRYTE_MAX, TRYTE_MAX, TRYTE_MAX])
-    );
-}
-
-fn into_i64(mut trytes: Vec<Tryte>) -> i64 {
-    let ternary = Ternary { trytes: &mut trytes };
-    ternary.into()
+    assert_eq!(WORD_MIN, Ternary::new(&mut TERNARY_MIN).into());
+    assert_eq!(-1i64, Ternary::new(&mut TERNARY_NEG1).into());
+    assert_eq!(0i64, Ternary::new(&mut TERNARY_0).into());
+    assert_eq!(1i64, Ternary::new(&mut TERNARY_1).into());
+    assert_eq!(WORD_MAX, Ternary::new(&mut TERNARY_MAX).into());
 }
 
 #[test]
 fn ternary_read_int() {
-    assert_eq!(
-        Ok(vec![TRYTE_MIN, TRYTE_MIN, TRYTE_MIN, TRYTE_MIN]),
-        trytes_from_i64(WORD_MIN)
-    );
-    assert_eq!(
-        Ok(vec![TRYTE_NEG1, TRYTE_0, TRYTE_0, TRYTE_0]),
-        trytes_from_i64(-1)
-    );
-    assert_eq!(
-        Ok(vec![TRYTE_0, TRYTE_0, TRYTE_0, TRYTE_0]),
-        trytes_from_i64(0)
-    );
-    assert_eq!(
-        Ok(vec![TRYTE_1, TRYTE_0, TRYTE_0, TRYTE_0]),
-        trytes_from_i64(1)
-    );
-    assert_eq!(
-        Ok(vec![TRYTE_MAX, TRYTE_MAX, TRYTE_MAX, TRYTE_MAX]),
-        trytes_from_i64(WORD_MAX)
-    );
+    assert_eq!(Ok(vec_from_slice(&TERNARY_MIN)), trytes_from_i64(WORD_MIN));
+    assert_eq!(Ok(vec_from_slice(&TERNARY_NEG1)), trytes_from_i64(-1));
+    assert_eq!(Ok(vec_from_slice(&TERNARY_0)), trytes_from_i64(0));
+    assert_eq!(Ok(vec_from_slice(&TERNARY_1)), trytes_from_i64(1));
+    assert_eq!(Ok(vec_from_slice(&TERNARY_MAX)), trytes_from_i64(WORD_MAX));
 
     assert!(trytes_from_i64(i64::min_value()).is_err());
     assert!(trytes_from_i64(i64::max_value()).is_err());
 }
 
+fn vec_from_slice<T: Clone>(slice: &[T]) -> Vec<T> {
+    let mut vec = Vec::new();
+    vec.extend_from_slice(slice);
+    vec
+}
+
 fn trytes_from_i64(n: i64) -> Result<Vec<Tryte>> {
     let mut trytes = vec![TRYTE_0, TRYTE_0, TRYTE_0, TRYTE_0];
-
-    {
-        let mut ternary = Ternary { trytes: &mut trytes };
-        ternary.read_int(n)?;
-    }
-
+    Ternary::new(&mut trytes).read_int(n)?;
     Ok(trytes)
 }
