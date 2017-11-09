add70c5301573a13eaf637360b3188e63c5ece4a
Jordan Danford <jordan@britecore.com>
2017-11-09 11:18:27 -0700

    Change a bunch of VM method names, remove unnnecessary mutability

--- src/vm.rs
+++ src/vm.rs
@@ -97,19 +97,19 @@ impl<'a> VM<'a> {
     }
 
     fn op_and(&mut self, operands: &operands::RRR) {
-        self.do_simple_rrr(operands, ternary::and);
+        self.simple_rrr(operands, ternary::and);
     }
 
     fn op_or(&mut self, operands: &operands::RRR) {
-        self.do_simple_rrr(operands, ternary::or);
+        self.simple_rrr(operands, ternary::or);
     }
 
     fn op_tmul(&mut self, operands: &operands::RRR) {
-        self.do_simple_rrr(operands, ternary::tmul);
+        self.simple_rrr(operands, ternary::tmul);
     }
 
     fn op_tcmp(&mut self, operands: &operands::RRR) {
-        self.do_simple_rrr(operands, ternary::tcmp);
+        self.simple_rrr(operands, ternary::tcmp);
     }
 
     fn op_cmp(&mut self, operands: &operands::RRR) {
@@ -126,7 +126,7 @@ impl<'a> VM<'a> {
 
     fn op_shf(&mut self, operands: &operands::RRR) {
         let offset = (&self.registers[operands.rhs]).into_i64() as isize;
-        self.do_shift(operands.lhs, offset);
+        self.shift(operands.lhs, offset);
         self.copy_shift_result(operands.dest);
     }
 
@@ -165,24 +165,24 @@ impl<'a> VM<'a> {
     }
 
     fn op_andi(&mut self, operands: &operands::RRI) {
-        self.do_simple_rri(operands, ternary::and);
+        self.simple_rri(operands, ternary::and);
     }
 
     fn op_ori(&mut self, operands: &operands::RRI) {
-        self.do_simple_rri(operands, ternary::or);
+        self.simple_rri(operands, ternary::or);
     }
 
     fn op_tmuli(&mut self, operands: &operands::RRI) {
-        self.do_simple_rri(operands, ternary::tmul);
+        self.simple_rri(operands, ternary::tmul);
     }
 
     fn op_tcmpi(&mut self, operands: &operands::RRI) {
-        self.do_simple_rri(operands, ternary::tcmp);
+        self.simple_rri(operands, ternary::tcmp);
     }
 
     fn op_shfi(&mut self, operands: &operands::RRI) {
         let offset = operands.immediate.into_i64() as isize;
-        self.do_shift(operands.src, offset);
+        self.shift(operands.src, offset);
         self.copy_shift_result(operands.dest);
     }
 
@@ -205,7 +205,7 @@ impl<'a> VM<'a> {
         let j = HALF_LEN * 2;
 
         {
-            let mut dest = &mut self.registers[operands.dest];
+            let dest = &mut self.registers[operands.dest];
             dest[0..i].clear();
             dest[i..j].copy_from_slice(&operands.immediate);
         }
@@ -223,85 +223,85 @@ impl<'a> VM<'a> {
     }
 
     fn op_lt(&mut self, operands: &operands::Memory) {
-        self.do_load(operands, TRYTE_LEN);
+        self.load(operands, TRYTE_LEN);
     }
 
     fn op_lh(&mut self, operands: &operands::Memory) {
-        self.do_load(operands, HALF_LEN);
+        self.load(operands, HALF_LEN);
     }
 
     fn op_lw(&mut self, operands: &operands::Memory) {
-        self.do_load(operands, WORD_LEN);
+        self.load(operands, WORD_LEN);
     }
 
     fn op_st(&mut self, operands: &operands::Memory) {
-        self.do_store(operands, TRYTE_LEN);
+        self.store(operands, TRYTE_LEN);
     }
 
     fn op_sh(&mut self, operands: &operands::Memory) {
-        self.do_store(operands, HALF_LEN);
+        self.store(operands, HALF_LEN);
     }
 
     fn op_sw(&mut self, operands: &operands::Memory) {
-        self.do_store(operands, WORD_LEN);
+        self.store(operands, WORD_LEN);
     }
 
     fn op_bt(&mut self, operands: &operands::Branch) {
         let selector = self.get_branch_selector(operands);
         let offset = self.get_branch_offset(operands);
-        self.do_branch(selector, offset, 0, 0);
+        self.branch(selector, offset, 0, 0);
     }
 
     fn op_b0(&mut self, operands: &operands::Branch) {
         let selector = self.get_branch_selector(operands);
         let offset = self.get_branch_offset(operands);
-        self.do_branch(selector, 0, offset, 0);
+        self.branch(selector, 0, offset, 0);
     }
 
     fn op_b1(&mut self, operands: &operands::Branch) {
         let selector = self.get_branch_selector(operands);
         let offset = self.get_branch_offset(operands);
-        self.do_branch(selector, 0, 0, offset);
+        self.branch(selector, 0, 0, offset);
     }
 
     fn op_bt0(&mut self, operands: &operands::Branch) {
         let selector = self.get_branch_selector(operands);
         let offset = self.get_branch_offset(operands);
-        self.do_branch(selector, offset, offset, 0);
+        self.branch(selector, offset, offset, 0);
     }
 
     fn op_bt1(&mut self, operands: &operands::Branch) {
         let selector = self.get_branch_selector(operands);
         let offset = self.get_branch_offset(operands);
-        self.do_branch(selector, offset, 0, offset);
+        self.branch(selector, offset, 0, offset);
     }
 
     fn op_b01(&mut self, operands: &operands::Branch) {
         let selector = self.get_branch_selector(operands);
         let offset = self.get_branch_offset(operands);
-        self.do_branch(selector, 0, offset, offset);
+        self.branch(selector, 0, offset, offset);
     }
 
     fn op_jmp(&mut self, operands: &operands::Jump) {
         let offset = self.get_jump_offset(operands);
-        self.do_rel_jump(offset);
+        self.jump_relative(offset);
     }
 
     fn op_call(&mut self, operands: &operands::Jump) {
         let offset = self.get_jump_offset(operands);
         self.save_pc();
-        self.do_rel_jump(offset);
+        self.jump_relative(offset);
     }
 
     fn op_jmpr(&mut self, operands: &operands::R) {
         let offset = self.get_r_offset(operands);
-        self.do_rel_jump(offset);
+        self.jump_relative(offset);
     }
 
     fn op_callr(&mut self, operands: &operands::R) {
         let offset = self.get_r_offset(operands);
         self.save_pc();
-        self.do_rel_jump(offset);
+        self.jump_relative(offset);
     }
 
     fn op_syscall(&mut self) {
@@ -312,7 +312,7 @@ impl<'a> VM<'a> {
         unimplemented!()
     }
 
-    fn do_simple_rrr<F>(&mut self, operands: &operands::RRR, f: F)
+    fn simple_rrr<F>(&mut self, operands: &operands::RRR, f: F)
     where
         F: Fn(&mut [Tryte], &[Tryte], &[Tryte]),
     {
@@ -328,7 +328,7 @@ impl<'a> VM<'a> {
         self.registers[registers::ZERO].clear();
     }
 
-    fn do_simple_rri<F>(&mut self, operands: &operands::RRI, f: F)
+    fn simple_rri<F>(&mut self, operands: &operands::RRI, f: F)
     where
         F: Fn(&mut [Tryte], &[Tryte], &[Tryte]),
     {
@@ -344,7 +344,7 @@ impl<'a> VM<'a> {
         self.registers[registers::ZERO].clear();
     }
 
-    fn do_shift(&mut self, src_reg: StandardRegister, offset: isize) {
+    fn shift(&mut self, src_reg: StandardRegister, offset: isize) {
         let tmp_dest = &mut self.scratch_space[0..(WORD_LEN * 3)];
         let src = &self.registers[src_reg];
         ternary::shift(tmp_dest, src, offset);
@@ -391,17 +391,17 @@ impl<'a> VM<'a> {
         src.get_trit(i)
     }
 
-    fn do_branch(&mut self, selector: Trit, offset_t: i32, offset_0: i32, offset_1: i32) {
+    fn branch(&mut self, selector: Trit, offset_t: i32, offset_0: i32, offset_1: i32) {
         self.jump_table[trit::NEG.into_index()] = offset_t;
         self.jump_table[trit::ZERO.into_index()] = offset_0;
         self.jump_table[trit::POS.into_index()] = offset_1;
 
         let i = selector.into_index();
         let offset = self.jump_table[i];
-        self.do_rel_jump(offset);
+        self.jump_relative(offset);
     }
 
-    fn do_load(&mut self, operands: &operands::Memory, len: usize) {
+    fn load(&mut self, operands: &operands::Memory, len: usize) {
         let i = self.get_memory_addr(operands);
         let j = i + len;
         let src = &self.memory[i..j];
@@ -409,14 +409,14 @@ impl<'a> VM<'a> {
         {
             let dest_reg = &mut self.registers[operands.dest];
             dest_reg.clear();
-            let mut dest = &mut dest_reg[..len];
+            let dest = &mut dest_reg[..len];
             dest.copy_from_slice(src);
         }
 
         self.registers[registers::ZERO].clear();
     }
 
-    fn do_store(&mut self, operands: &operands::Memory, len: usize) {
+    fn store(&mut self, operands: &operands::Memory, len: usize) {
         let i = self.get_memory_addr(operands);
         let j = i + len;
 
@@ -435,19 +435,19 @@ impl<'a> VM<'a> {
     }
 
     fn get_jump_offset(&mut self, operands: &operands::Jump) -> i32 {
-        let mut offset_dest = &mut self.scratch_space[0..WORD_LEN];
+        let offset_dest = &mut self.scratch_space[0..WORD_LEN];
         offset_dest.copy_from_slice(&operands.offset[..]);
         offset_dest.into_i64() as i32
     }
 
     fn get_branch_offset(&mut self, operands: &operands::Branch) -> i32 {
-        let mut offset_dest = &mut self.scratch_space[0..HALF_LEN];
+        let offset_dest = &mut self.scratch_space[0..HALF_LEN];
         offset_dest.copy_from_slice(&operands.offset[..]);
         offset_dest.into_i64() as i32
     }
 
     fn get_memory_offset(&mut self, operands: &operands::Memory) -> i32 {
-        let mut offset_dest = &mut self.scratch_space[0..HALF_LEN];
+        let offset_dest = &mut self.scratch_space[0..HALF_LEN];
         offset_dest.copy_from_slice(&operands.offset[..]);
         offset_dest.into_i64() as i32
     }
@@ -463,11 +463,11 @@ impl<'a> VM<'a> {
             .expect("ternary arithmetic error")
     }
 
-    fn do_rel_jump(&mut self, offset: i32) {
-        self.pc = self.rel_pc(offset);
+    fn jump_relative(&mut self, offset: i32) {
+        self.pc = self.relative_pc(offset);
     }
 
-    fn rel_pc(&self, offset: i32) -> u32 {
+    fn relative_pc(&self, offset: i32) -> u32 {
         (self.pc as i32 + offset) as u32
     }
 }
